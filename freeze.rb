e35bbb24f905e0293b1ca6452e5123d4db08ef42
Jason Carpentier <jcarpentier@gmail.com>
2011-10-14 11:46:57 -0500

    Added handling of places in URL

--- freeze.rb
+++ freeze.rb
@@ -3,6 +3,7 @@ require 'sinatra'
 require 'sinatra/flash'
 require 'barometer'
 require 'erb'
+require 'uri'
 
 enable :sessions
 
@@ -21,6 +22,23 @@ get '/' do
   erb :index
 end
 
+get '/:place' do
+  begin
+    place = params[:place]
+    barometer = Barometer.new(place)
+    weather = barometer.measure
+    @today_low_f = weather.today.low.to_i
+    @today_low_c = (@today_low_f - 32) * 5/9
+    @tomorrow_low_f = weather.tomorrow.low.to_i
+    @tomorrow_low_c = (@tomorrow_low_f - 32) * 5/9
+    @location = weather.default.location.city
+    @site = URI.escape("http://willitfreezetonight.com/#{@location}")
+  rescue ArguementError
+    flash["alert-message error"] = "Either this place doesn't exist or we don't have access to weather info there.  Try searching for a nearby town or zipcode. <br /> Contact me @JasonCarpentier to report"
+  end
+  erb :weather
+end
+
 post '/weather' do
   begin
     place = params[:query]
@@ -31,7 +49,7 @@ post '/weather' do
     @tomorrow_low_f = weather.tomorrow.low.to_i
     @tomorrow_low_c = (@tomorrow_low_f - 32) * 5/9
     @location = weather.default.location.city
-
+    @site = URI.escape("http://willitfreezetonight.com/#{@location}")
     rescue ArgumentError
       if place.downcase == "hell"
         @hell = "Probably not, but if the Cowboys won today... Yes and there is snow too!"
