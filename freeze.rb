76caafe933791d8b8a1cde576cc02b46bc4a6f24
Jason Carpentier <jcarpentier@gmail.com>
2011-10-22 00:06:25 -0500

    testing twilio again

--- freeze.rb
+++ freeze.rb
@@ -28,10 +28,7 @@ get '/' do
 end
 
 post '/mobile' do
-  @twilio_client.account.sms.messages.create(
-    :from => '+12106512991',
-    :to => '+12107751266',
-    :body => 'Got it!')
+  @twilio_client.account.sms.messages.create(:from => '+12106512991', :to => '+12107751266',:body => 'Got it!')
 end
 
 get '/:place' do
