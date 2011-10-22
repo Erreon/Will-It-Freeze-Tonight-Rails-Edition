c6cca54b9b21c7978e08a6c53963bf46e69b6c07
Jason Carpentier <jason@jason-Studio-XPS-9100.(none)>
2011-10-22 15:02:18 -0500

    Rails 3.1 rewrite

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Freezetonight::Application.config.secret_token = 'e927cfa7dfeb22acc893fecebeb9e3af696b3c3a4306d9f56a5237a9d4054e09b13fba183f7919a31dfab3066691716e64534a4096f075064f36111236ad0971'
