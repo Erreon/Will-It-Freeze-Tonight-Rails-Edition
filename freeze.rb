6c70fb8ca2e385ec9e57353d0959ceef0b5c2338
Jason Carpentier <jason@jason-Studio-XPS-9100.(none)>
2011-10-22 03:29:34 -0500

    test

--- freeze.rb
+++ freeze.rb
@@ -8,10 +8,25 @@ require 'uri'
 enable :sessions
 
 helpers do
+  def check_weather(place)
+    begin
+      barometer = Barometer.new(place)
+      weather = barometer.measure
+      @today_low_f = weather.today.low.to_i
+      @today_low_c = (@today_low_f - 32) * 5/9
+      @tomorrow_low_f = weather.tomorrow.low.to_i
+      @tomorrow_low_c = (@tomorrow_low_f - 32) * 5/9
+      @location = weather.default.location.city
+      @site = URI.escape("http://willitfreezetonight.com/#{@location}")
+    rescue ArgumentError
+      flash["alert-message error"] = "Either this place doesn't exist or we don't have access to weather info there.  Try searching for a nearby town or zipcode. <br /> Contact me @JasonCarpentier to report any issues."
+      redirect '/'
+    end
+  end
   def freezing?(temp)
     if temp <= 32
       true
-    else
+    else 
       false
     end
   end
@@ -23,44 +38,14 @@ get '/' do
 end
 
 get '/:place' do
-  begin
-    place = params[:place]
-    barometer = Barometer.new(place)
-    weather = barometer.measure
-    @today_low_f = weather.today.low.to_i
-    @today_low_c = (@today_low_f - 32) * 5/9
-    @tomorrow_low_f = weather.tomorrow.low.to_i
-    @tomorrow_low_c = (@tomorrow_low_f - 32) * 5/9
-    @location = weather.default.location.city
-    @site = URI.escape("http://willitfreezetonight.com/#{@location}")
-  rescue ArguementError
-    flash["alert-message error"] = "Either this place doesn't exist or we don't have access to weather info there.  Try searching for a nearby town or zipcode. <br /> Contact me @JasonCarpentier to report"
-  end
+  check_weather(params[:place])
   erb :weather
 end
 
 post '/weather' do
-  begin
-    place = params[:query]
-    barometer = Barometer.new(place)
-    weather = barometer.measure
-    @today_low_f = weather.today.low.to_i
-    @today_low_c = (@today_low_f - 32) * 5/9
-    @tomorrow_low_f = weather.tomorrow.low.to_i
-    @tomorrow_low_c = (@tomorrow_low_f - 32) * 5/9
-    @location = weather.default.location.city
-    @site = URI.escape("http://willitfreezetonight.com/#{@location}")
-    rescue ArgumentError
-      if place.downcase == "hell"
-        @hell = "Probably not, but if the Cowboys won today... Yes and there is snow too!"
-      else
-        flash["alert-message error"] = "Either this place doesn't exist or we don't have access to weather info there.  Try searching for a nearby town or zipcode. <br /> Contact me @JasonCarpentier to suggest a place."
-        redirect '/'
-      end
-  end
+  check_weather(params[:query])
   erb :weather
 end
-
 not_found do
   redirect '/'
 end
